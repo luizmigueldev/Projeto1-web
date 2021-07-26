eb80c005f1a5f4c959197baa357f345f3e91279b
luizmigueldev <luizmigueln.dev@gmail.com>
2021-07-26 20:51:16 -0300

    Criação de arquivo Js separado do HTML, e inicialização da captura de dados do Formulario

---
+++ script/scripts.js
@@ -0,0 +1,12 @@
+
+const formLogin = document.getElementById("form");
+
+formLogin.addEventListener("submit", submit)
+
+async function submit(event) {
+
+  event.preventDefault();
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+}
