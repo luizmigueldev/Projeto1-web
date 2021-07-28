813c6e375410446d618fcb305f53aa39289c8514
Bruno Mantovani <brunomoraesdmc@gmail.com>
2021-07-27 22:48:20 -0300

    Realizando request na API

--- script/scripts.js
+++ script/scripts.js
@@ -1,14 +1,18 @@
 const token = localStorage.getItem("token");
 const containerInf = document.getElementById('container-inf');
+const contentApi = document.getElementById('content-api');
+const formApi = document.getElementById("form-api");
 
 
 if (token) {
   containerInf.style.display = 'none';
+  contentApi.style.display = 'flex';
 
 }
 
 const formLogin = document.getElementById("form");
 formLogin.addEventListener("submit", submit)
+formApi.addEventListener("submit", requestApi)
 
 async function submit(event) {
 
@@ -24,6 +28,7 @@ async function submit(event) {
     if (status == 200) {
       localStorage.setItem("token", data.token);
       containerInf.style.display = 'none';
+      contentApi.style.display = 'flex';
     }
   }
   catch (e) {
@@ -31,4 +36,28 @@ async function submit(event) {
     error.textContent = "Usuario e/ou Senha Invalidos!!"
 
   }
+}
+async function requestApi(event) {
+  event.preventDefault();
+  const query = document.getElementById("query").value;
+
+  try {
+    const key = "8802371-6f1e7f4cccc616e9ab78e5fd2";
+
+    const urlRequest = `https://pixabay.com/api/?key=${key}&q=${encodeURI(query)}&image_type=photo&per_page`
+    const { data, status } = await axios.get(urlRequest)
+    console.log(data)
+    if (status == 200) {
+      const containerImages = document.querySelector(".image-busca")
+      containerImages.innerHTML = '';
+      data.hits.map(({ previewURL }) => {
+        const image = document.createElement(`img`);
+        image.src = previewURL;
+        containerImages.appendChild(image);
+      })
+    }
+  } catch (e) {
+    console.log(e)
+  }
+
 }
